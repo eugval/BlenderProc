119f29f3df363db7028560ac73e6733f60147ca8
Eugene <peugval@gmail.com>
2021-04-17 13:52:29 +0100

    adding for many coffee cups dataset

--- make_dataset.py
+++ make_dataset.py
@@ -13,8 +13,8 @@ for i in range(3000):
     print('ITERATION {}'.format(i))
 
     # os.environ["BLENDER_PROC_RANDOM_SEED"] = str(int(start_time*10000000)+i)
-    os.environ["BLENDER_PROC_RANDOM_SEED"] = str(1232)
-    cmd = ["python", "run.py", "examples/OneObj/config.yaml", "examples/OneObj/output" , " resources/ModelNet40/"," ~/Projects/BlenderProc/resources/cctextures/"]
+    # os.environ["BLENDER_PROC_RANDOM_SEED"] = str(1232)
+    cmd = ["python", "run.py", "examples/simple_coffee_mugs/config.yaml", "examples/simple_coffee_mugs/output" , "resources/ModelNet40/"]
     subprocess.check_output(cmd)
 
     print("Done in : {}hrs".format((time.time()-start_time)/3600.))
