7f0a7822d1dc8f63650a3bbaa9631b6cac3e1c5a
Eugene <peugval@gmail.com>
2021-04-23 19:24:57 +0100

    changes for full monochrome randomisation

--- make_dataset.py
+++ make_dataset.py
@@ -9,12 +9,12 @@ temp_output = "current_temp_output"
 file_counter = 0
 start_time = time.time()
 print('start_time : {}'.format(start_time))
-for i in range(3000):
+for i in range(750):
     print('ITERATION {}'.format(i))
 
     # os.environ["BLENDER_PROC_RANDOM_SEED"] = str(int(start_time*10000000)+i)
     # os.environ["BLENDER_PROC_RANDOM_SEED"] = str(1232)
-    cmd = ["python", "run.py", "examples/MultiObj2/config.yaml", "examples/MultiObj2/output" , "~/Projects/BlenderProc/resources/ModelNet40/"]
+    cmd = ["python", "run.py", "examples/MultiObj2/config.yaml", "examples/MultiObj2/output" , "~/Projects/BlenderProc/resources/ModelNet40/", "10"]
     subprocess.check_output(cmd)
 
     print("Done in : {}hrs".format((time.time()-start_time)/3600.))
